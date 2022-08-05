55580104be13b4525cd0462811264d0f789b8e7d
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-08-05 23:02:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,29 @@ import Container from './Container/Container';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import { nanoid } from 'nanoid';
-const contactsStart = [
+const defaultContacts = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
+
 export default function App() {
-  const [contacts, setContacts] = useState([...contactsStart] ?? []);
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? defaultContacts;
+  });
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
+
+  useEffect(() => {
+    return window.localStorage.setItem(
+      'contacts',
+      JSON.stringify(defaultContacts)
+    );
+  }, []);
 
   const formSubmitHandle = data => {
     const sameName = contacts.map(contact => contact.name).includes(data.name);
